16ddc71b8dda3f18f9ed8be2aaa17d607e8c603c
akil <akil@everycrave.me>
2019-12-09 18:24:33 +0530

    bug fixes

--- src/android/Autostart.java
+++ src/android/Autostart.java
@@ -48,17 +48,17 @@ public class Autostart extends CordovaPlugin {
         if(action.equalsIgnoreCase("isAutoStartRequired")){
             String manufacturer = android.os.Build.MANUFACTURER;
 
-            boolean autoStartRequired = false;
+            String autoStartRequired = 'false';
 
             switch (manufacturer) {
                 case "xiaomi":
-                    autoStartRequired = true;
+                    autoStartRequired = 'true';
                     break;
                 case "oppo":
-                    autoStartRequired = true;
+                    autoStartRequired = 'true';
                     break;
                 case "vivo":
-                    autoStartRequired = true;
+                    autoStartRequired = 'true';
                     break;
             }
             callbackContext.success(autoStartRequired);   
--- www/autostart.js
+++ www/autostart.js
@@ -4,17 +4,12 @@ var channel = require('cordova/channel');
 var exec = require('cordova/exec');
 var cordova = require('cordova');
 
-channel.createSticky('onCordovaInfoReady');
-// Tell cordova channel to wait on the CordovaInfoReady event
-channel.waitForInitialization('onCordovaInfoReady');
 
-
-module.exports = {
-    doesDeviceNeedAutoStart: function (successCallback, errorCallback) {
-        exec(successCallback, errorCallback, 'isAutoStartRequired', 'isAutoStartRequired', []);
-    };
-
-    isAutoStartRequired: function(successCallback, errorCallback){
-        exec(successCallback, errorCallback, 'showAutoStartSettings', 'showAutoStartSettings', []);
-    }
+window.doesDeviceNeedAutoStart = function( callback) {
+    exec(successCallback, errorCallback, 'isAutoStartRequired', 'isAutoStartRequired', []);
 };
+
+
+window.isAutoStartRequired = function(successCallback, errorCallback){
+    exec(successCallback, errorCallback, 'showAutoStartSettings', 'showAutoStartSettings', []);
+}
